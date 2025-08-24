aacef4e1f85a8f0287f65d24b57321cb92b14d5e
IsaacLopezz <islopezgo@est.utn.ac.cr>
2025-08-24 14:36:10 -0600

    Paquetes Modelo,Vista,Controlador y Exceptions
    Se crean todos estos paquetes pero unicamente se trabaja en el paquete exceptions creando tres clases con cierto codigo para exceptions del proyecto

---
+++ src/Exceptions/ValidacionException.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author ilope
+ */
+public class ValidacionException extends Exception {
+    public ValidacionException(String mensaje) 
+    { super(mensaje); }
+}
